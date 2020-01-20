b921c6501f435b256996e4fb452b358b74f0fe6f
Mouaad <boukiaou.mouaad@gmail.com>
2020-01-21 00:53:13 +0100

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  siteMetadata: {
+    title: `Tree Project`,
+    description: `Tree Project`,
+    author: `Mouaad Boukiaou`
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Tree Project`,
+        short_name: `Tree Project`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/icon.png`
+      }
+    }
+  ]
+};
