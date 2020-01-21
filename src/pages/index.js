4d9397cc24637dee16e909300614ca9ec56d4afa
Mouaad <boukiaou.mouaad@gmail.com>
2020-01-21 06:48:14 +0100

    last commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,20 @@
 import React from "react";
-import { Redirect } from "@reach/router";
+import { Link } from "gatsby";
+import styled from "styled-components";
+const Container = styled.div`
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  a {
+    font-size: 32px;
+  }
+`;
 const IndexPage = () => {
-  return <Redirect to="/az1122401/ " />;
+  return (
+    <Container>
+      <Link to="./az1122401">Go the Main page</Link>
+    </Container>
+  );
 };
 export default IndexPage;
